cbccc519d61af77543e3f869634038ee4d8f127a
Thu Tran <chiujde6737@gmail.com>
2023-12-25 02:31:10 +0700

    chanqua

---
+++ src/haigiosnag/ngoichill.java
@@ -0,0 +1,7 @@
+package haigiosnag;
+
+public class ngoichill {
+	public static void main(String[] args) {
+		System.out.print("thu");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module haigiosnag {
+}
